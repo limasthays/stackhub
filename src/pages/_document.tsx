3cad1ae84ce3a37aae9481ac05c68b0180df678d
User <limasthays@gmail.com>
2023-04-12 22:58:16 -0300

    initial commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="pt-br">
+			<Head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600;700;900&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
 }
