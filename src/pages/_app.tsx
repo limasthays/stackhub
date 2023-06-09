93cf39fabb3a14c6ef5e0c27a0d2daeda67b0216
User <limasthays@gmail.com>
2023-06-08 23:31:04 -0300

    adding toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<ToastContainer
+				position="top-right"
+				autoClose={4000}
+				newestOnTop
+				closeOnClick
+				pauseOnHover
+				theme="dark"
+			/>
+			<Component {...pageProps} />
+		</>
+	)
 }
