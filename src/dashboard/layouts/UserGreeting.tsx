650fe4acad18623cad3f6f51a1e229dcfff7ffdd
User <limasthays@gmail.com>
2023-06-12 23:00:10 -0300

    giving user greeting texts some space

--- src/dashboard/layouts/UserGreeting.tsx
+++ src/dashboard/layouts/UserGreeting.tsx
@@ -5,11 +5,11 @@ export function UserGreeting() {
 	const { userData } = useContext(DashboardContentContext)
 
 	return (
-		<div className="py-8">
+		<div className="py-8 flex flex-col gap-2 md:flex-row md:justify-between md:items-center md:gap-0">
 			<h3 className="text-neutral-50 font-bold text-lg">
 				Olá, {userData?.name}
 			</h3>
-			<p>{userData?.course_module}</p>
+			<p className="text-xs text-neutral-500">{userData?.course_module}</p>
 		</div>
 	)
 }
