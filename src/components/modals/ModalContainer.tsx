5cc241eeb842dc44aa8285a74e356e8b63d9757a
User <limasthays@gmail.com>
2023-07-05 02:50:28 -0300

    modal width in md screen

--- src/components/modals/ModalContainer.tsx
+++ src/components/modals/ModalContainer.tsx
@@ -25,9 +25,9 @@ export function ModalContainer({
 			id="modal"
 			className="bg-neutral-900/50 absolute top-0 left-0 w-full h-full flex items-center justify-center"
 		>
-			<section className="mx-3 w-full max-w-[300px]">
-				<div className="flex justify-between items-center bg-neutral-700 px-4 py-2 rounded-t">
-					<span className="text-sm">{title}</span>
+			<section className="mx-3 w-full max-w-[300px] md:max-w-[380px]">
+				<div className="flex justify-between items-center bg-neutral-700 px-4 py-3 rounded-t">
+					<span className="text-sm font-bold">{title}</span>
 					<button className="font-bold bg-neutral-700" onClick={onClose}>
 						<MdClose />
 					</button>
